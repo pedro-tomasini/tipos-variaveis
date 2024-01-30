491eab78868f206c6a66276a0494a739007f2ffd
Pedro Tomasini <pedrotomasini1@gmail.com>
2024-01-30 00:35:04 -0300

    tipos e variáveis

--- src/TposVariaveis.java
+++ src/TposVariaveis.java
@@ -1,5 +1,27 @@
 public class TposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Mundo!");
+     public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;                
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+            // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        
+        //Variáveis e Constantes
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+       final double VALOR_DE_PI = 3.14;
+
+       
     }
 }
