ab9f45ebec034b8a46d96ec54ddb44426f4993f9
Pedro Tomasini <pedrotomasini1@gmail.com>
2024-01-29 23:07:09 -0300

    estudo de tipos e variaveis

--- src/TposVariaveis.java
+++ src/TposVariaveis.java
@@ -1,5 +1,5 @@
 public class TposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Olá, Mundo!");
     }
 }
